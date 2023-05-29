0a432f7e2417bb7875cea707412a13f219d281f7
Eric Nelson <big89e@gmail.com>
2023-05-29 12:45:44 -0400

    Add more save states

---
+++ megaman2/savestates/00-difficult/cp-test-runs.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+DEST="../../../../fceux/fcs/Mega Man 2 (U).fc"
+
+CMD="cp"
+${CMD} 02b-heatman-level.fc0 "${DEST}1"
+${CMD} 03a-airman-clouds.fc0 "${DEST}2"
+${CMD} 04a-crashman-level.fc0 "${DEST}3"
+${CMD} 05-quickman.fc0 "${DEST}4"
+${CMD} 08a-bubbleman-level.fc0 "${DEST}5"
+${CMD} 08c-metalman-l3.fc0 "${DEST}6"
+${CMD} 10-blocks.fc0 "${DEST}7"
+${CMD} 14b-alien.fc0 "${DEST}8"
+${CMD} 12-bbeam-drop.fc0 "${DEST}9"
+${CMD} 12-bbeam-boss.fc0 "${DEST}0"
+
+
